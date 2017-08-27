bbbfb0dbe951c852a6c84dbe44bdb158fe37a190
irfanalidv <irfanali29@hotmail.com>
2017-08-27 20:18:34 +0530

    annotate #7

--- OpenNLP/src/Apache OpenNLP based chunk annotators.R
+++ OpenNLP/src/Apache OpenNLP based chunk annotators.R
@@ -10,3 +10,11 @@ s <- as.String(s)
 
 sent_token_annotator <- Maxent_Sent_Token_Annotator()
 word_token_annotator <- Maxent_Word_Token_Annotator()
+pos_tag_annotator <- Maxent_POS_Tag_Annotator()
+
+a3 <- annotate(s,
+               list(sent_token_annotator,
+                    word_token_annotator,
+                    pos_tag_annotator))
+
+
