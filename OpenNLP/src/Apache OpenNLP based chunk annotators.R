d1f79ef38cb44350bfefa941ab206683d6f91db0
irfanalidv <irfanali29@hotmail.com>
2017-08-27 19:54:58 +0530

    Maxent_Word_Token_Annotator #7

--- OpenNLP/src/Apache OpenNLP based chunk annotators.R
+++ OpenNLP/src/Apache OpenNLP based chunk annotators.R
@@ -7,3 +7,6 @@ s <- paste(c("Pierre Vinken, 61 years old, will join the board as a ",
               collapse = "") 
 
 s <- as.String(s)
+
+sent_token_annotator <- Maxent_Sent_Token_Annotator()
+word_token_annotator <- Maxent_Word_Token_Annotator()
